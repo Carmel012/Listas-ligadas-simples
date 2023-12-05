c90755e4787bb14c305bfedebe5f73742eb5f67f
Carmel012 <152313246+Carmel012@users.noreply.github.com>
2023-12-04 19:02:49 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.listasimple;
+
+/**
+ *
+ * @author carmelorodriguezchab
+ */
+public class Main {
+    public static void main(String[] args) {
+        ListaSimple Lista = new ListaSimple();
+
+        Lista.insert(3); 
+        Lista.insert(7);
+        Lista.insert(1);
+        Lista.insert(5);
+
+        System.out.println("Lista original:");
+        Lista.display();
+
+        int searchKey = 1;
+        Node foundNode = Lista.searchByKey(searchKey);
+        System.out.println("Nodo con clave " + searchKey + " encontrado: " + (foundNode != null));
+
+        int insertKey = 7;
+        int newKey = 10;
+        Lista.insertAfter(insertKey, newKey);
+        System.out.println("Lista después de insertar " + newKey + " después de " + insertKey + ":");
+        Lista.display();
+
+        int key1 = 5;
+        int key2 = 3;
+        Lista.swapNodes(key1, key2);
+        System.out.println("Lista después de intercambiar nodos con claves " + key1 + " y " + key2 + ":");
+        Lista.display();
+    }
+}
---
+++ Node.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.listasimple;
+
+/**
+ *
+ * @author carmelorodriguezchab
+ */
+class Node {
+    
+    int key;
+    Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
